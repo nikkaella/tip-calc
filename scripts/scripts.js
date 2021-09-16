226e2875580bed84f0fcac8b68735d252f3013b8
Nikka Ella <nedyalung@ucdavis.edu>
2021-09-15 17:11:41 -0700

    script update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,16 +3,18 @@ var numOfPeople = $('#numOfPeople');
 var tipAmt = $('#tip-amt');
 var total = $('#total');
 var custom = $('#custom');
+var percent;
 
 $(document).ready(clearInput);
 
 $('#bill').change(function () {
-    var bills = bill.val();
+    bill = parseInt(bill.val());
 });
 
 $('.butt').on('click', function () {
     $(this).toggleClass('clicked');
-
+    percent = parseInt($(this).text());
+    percent = percent * 0.01;
     // if there is already a button that is clicked, then remove previous one
 });
 
@@ -22,6 +24,11 @@ $('#custom').on('click', function() {
 
 $('#numOfPeople').change(function () {
     // alert(numOfPeople + bill);
+    numOfPeople = parseInt(numOfPeople.val());
+
+    tipAmt = (bill + (bill * percent)) / numOfPeople;
+    alert(tipAmt);
+    // tipAmt.text(tipAmt);
 });
 
 $('#reset').on('click', clearInput);
