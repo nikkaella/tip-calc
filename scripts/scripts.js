37bf8e85317ee961a584d4d1236243ac31bc0b5f
Nikka Ella <nedyalung@ucdavis.edu>
2021-09-15 17:51:20 -0700

    90% done

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,36 +3,46 @@ var numOfPeople = $('#numOfPeople');
 var tipAmt = $('#tip-amt');
 var total = $('#total');
 var custom = $('#custom');
+var customTip;
 var percent;
 var bills;
 var numPeople;
+
 $(document).ready(clearInput);
+
+$(document).on('keyup keypress', 'form input[type="text"]', function(e) {
+    if(e.keyCode == 13) {
+      e.preventDefault();
+      return false;
+    }
+  });
 
 $('#bill').change(function () {
     bills = parseInt(bill.val());
 });
 
 $('.butt').on('click', function () {
+    $('.butt').removeClass('clicked');
+
     $(this).toggleClass('clicked');
     percent = parseInt($(this).text());
     percent = percent * 0.01;
-    // if there is already a button that is clicked, then remove previous one
+
 });
 
-$('#custom').on('click', function () {
-    custom.val('');
-    percent = parseInt(custom.val(''));
-    console.log(percent);
+$('#custom').change(function () {
+
+    customTip = parseInt(custom.val());
+    percent = customTip * 0.01;
 })
 
 $('#numOfPeople').change(function () {
-    // alert(numOfPeople + bill);
     numPeople = parseInt(numOfPeople.val());
     
     var tip = (bills * percent) / numPeople;
     var totes = (bills / numPeople) + tip;
-    tipAmt.text('$' + tip);
-    total.text('$' + totes);
+    tipAmt.text('$' + tip.toFixed(2));
+    total.text('$' + totes.toFixed(2));
 });
 
 $('#reset').on('click', clearInput);
@@ -44,5 +54,6 @@ function clearInput() {
     numOfPeople.val('');
     tipAmt.text('$0.00');
     total.text('$0.00');
+    custom.val('');
     $('.butt').removeClass('clicked');
 }
